15f22872738ce264856c93ef82058956f7e693c2
lemn <zorelemn@zju.edu.cn>
2019-05-20 13:07:27 +0800

    change preception

--- preception.py
+++ preception.py
@@ -1,19 +1,22 @@
-#import numpy as np
+import numpy as np
 
-class preception()：
-	def __init__(X,Y,lr=0.01):
+class preception():
+	def __init__(self,X,Y,lr=0.01,N=20):
 		self.X = X
 		self.Y = Y
 		self.fea_dim = X.shape[1]
 		####initialize w and b
-		self.w = np.random.uniform(self.fea_dim)
+		self.w = np.random.uniform(size=self.fea_dim)
 		self.b = np.random.random()
 		self.lr = lr
+		self.N = N
 
-	def fit():
+	def fit(self):
 		i = 0
-		while True:
-			y = self.w*self.X.T+self.b
+		k = self.N
+		while True and k!=0:
+			k=k-1
+			y = np.matmul(self.X,self.w)+self.b
 			y_sign = np.sign(y)
 			y_mis = (self.Y!=y_sign)
 			loss = np.sum(-self.Y[y_mis]*y[y_mis])
@@ -24,5 +27,7 @@ class preception()：
 			self.w = self.w+self.lr*self.Y[i]*self.X[i,:]
 			self.b = self.b+self.lr*self.Y[i]
 
-	def predict(data):
-		
+	def predict(self,data):
+		y = np.matmul(data,self.w)+self.b
+		return np.sign(y)
+
